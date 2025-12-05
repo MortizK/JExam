52a4e251da81c39189d4edbc51ea53bab144dbfb
Moritz Köhler <moritz-koehler@freenet.de>
2025-12-05 15:42:57 +0100

    Initial Commit: Created with the Intellij JavaFX groundwork

---
+++ src/main/java/dhbw/koehler/jexaminer/HelloController.java
@@ -0,0 +1,14 @@
+package dhbw.koehler.jexaminer;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module dhbw.koehler.jexaminer {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens dhbw.koehler.jexaminer to javafx.fxml;
+    exports dhbw.koehler.jexaminer;
+}
