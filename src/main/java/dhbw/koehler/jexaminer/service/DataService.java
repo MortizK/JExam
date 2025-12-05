e4e8de4c86a98646b0c7e953d6e870837a86db70
Moritz Köhler <moritz-koehler@freenet.de>
2025-12-05 17:43:12 +0100

    Added BreadCrumb Data

--- src/main/java/dhbw/koehler/jexaminer/service/DataService.java
+++ src/main/java/dhbw/koehler/jexaminer/service/DataService.java
@@ -3,21 +3,36 @@ package dhbw.koehler.jexaminer.service;
 import dhbw.koehler.jexaminer.model.*;
 import dhbw.koehler.jexaminer.model.enums.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DataService {
     public Exam exam;
     public Object selectedItem;
     public Type type;
+    public List<Integer> path;
+    public List<String> breadCrumbs;
 
     public DataService(String name) {
         this.exam = new Exam((name == null) ? "Exam" : name);
         this.selectedItem = this.exam;
         this.type = Type.EXAM;
+        this.path = new ArrayList<>();
+        this.breadCrumbs = new ArrayList<>();
+        this.breadCrumbs.add(this.exam.getName());
         createExampleData();
     }
 
     public void updateSelectedFromPath(List<Integer> path) {
+        this.path = path;
+
+        // Also update the breadCrumbs
+        this.breadCrumbs = new ArrayList<>();
+        breadCrumbs.add(this.exam.getName());
+
+        Chapter selectedChapter;
+
+        // Logic for nested Structure, limited to Tasks
         switch(path.size()) {
             case 0:
                 this.type = Type.EXAM;
@@ -25,14 +40,21 @@ public class DataService {
                 break;
             case 1:
                 this.type = Type.CHAPTER;
-                this.selectedItem = this.exam.getChapters().get(path.getFirst());
+                selectedChapter = this.exam.getChapters().get(path.getFirst());
+                breadCrumbs.add(selectedChapter.getName());
+                this.selectedItem = selectedChapter;
                 break;
             case 2:
                 this.type = Type.TASK;
-                Chapter selectedChapter = this.exam.getChapters().get(path.getFirst());
-                this.selectedItem = selectedChapter.getTasks().get(path.get(1));
+                selectedChapter = this.exam.getChapters().get(path.getFirst());
+                breadCrumbs.add(selectedChapter.getName());
+                Task selectedTask = selectedChapter.getTasks().get(path.get(1));
+                breadCrumbs.add(selectedTask.getName());
+                this.selectedItem = selectedTask;
                 break;
         }
+
+        System.out.println(breadCrumbs.toString());
     }
 
     public void createExampleData() {
@@ -43,8 +65,8 @@ public class DataService {
 
         Task task1 = new Task("task1", variant1, 5, Difficulty.EASY, Scope.EXAM);
         task1.addVariant(variant4);
-        Task task2 = new Task("task1", variant2, 5, Difficulty.EASY, Scope.EXAM);
-        Task task3 = new Task("task1", variant3, 5, Difficulty.EASY, Scope.EXAM);
+        Task task2 = new Task("task2", variant2, 5, Difficulty.EASY, Scope.EXAM);
+        Task task3 = new Task("task3", variant3, 5, Difficulty.EASY, Scope.EXAM);
 
         Chapter chapter1 = new Chapter("chapter1");
         chapter1.addTask(task1);
